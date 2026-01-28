8132e18df911c3820964bbf9958e8541d7461389
Shovalshar <shovalshar02@gmail.com>
2026-01-28 22:04:01 +0200

    add delete comment by id route

--- src/routes/commentRoutes.ts
+++ src/routes/commentRoutes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/", commentController.create.bind(commentController));
 router.get("/:id", commentController.getById.bind(commentController));
 router.put("/:id", commentController.put.bind(commentController));
+router.delete("/:id", commentController.delete.bind(commentController));
 
 export default router;

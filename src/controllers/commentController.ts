8fdef867822962663da99a99333b505e95240e85
Daniel <dandany123@gmail.com>
2026-01-29 01:21:14 +0200

    added usage of user from token in comments

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,15 +1,35 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import baseController from "./baseController";
 import PostModel from "models/postModel";
 import CommentModel from "models/commentModel";
+import { AuthRequest } from "middlewares/authMiddleware";
 
 class CommentController extends baseController {
     constructor() {
         super(CommentModel);
     }
 
-    async create(req: Request, res: Response) {
+
+    async delete(req: AuthRequest, res: Response) {
+        const id = req.params.id;
+        const userId = req.user?._id;
+        try {
+            const comment = await CommentModel.findById(id);
+            if (!comment) {
+                return res.status(404).json({ error: "Comment not found" });
+            }
+            if (comment.sender !== userId) {
+                return res.status(403).json({ error: "Unauthorized" });
+            }
+            return super.delete(req, res);
+        } catch (error) {
+            this.handleError(res, error);
+        }
+    }
+
+    async create(req: AuthRequest, res: Response) {
         const postId = req.body.postId;
+        req.body.sender = req.user?._id;
         try {
             const post = await PostModel.findById(postId);
             if (!post) {
@@ -18,8 +38,26 @@ class CommentController extends baseController {
             }
             super.create(req, res);
         } catch (error) {
-            console.log(error);
-            res.status(500).send("An unknown error occurred while verifying post existence");
+            this.handleError(res, error);
+        }
+    }
+
+    async put(req: AuthRequest, res: Response) {
+        const id = req.params.id;
+        const userId = req.user?._id;
+        try {
+            const comment = await CommentModel.findById(id);
+            if (!comment) {
+                res.status(404).json({ error: "Comment not found" });
+                return;
+            }
+            if (comment.sender !== userId) {
+                res.status(403).json({ error: "Unauthorized" });
+                return;
+            }
+            super.put(req, res);
+        } catch (error) {
+            this.handleError(res, error);
         }
     }
 
